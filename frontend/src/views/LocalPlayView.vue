392263915b2be4014b542b894f6ec666da37975b
Luan-D-M <digjohnsons@gmail.com>
2026-02-18 20:34:35 -0300

    Adds Undo button

--- frontend/src/views/LocalPlayView.vue
+++ frontend/src/views/LocalPlayView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import { TheChessboard, type BoardApi } from 'vue3-chessboard';
+import { TheChessboard as Chessboard, type BoardApi } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 
 let boardApi: BoardApi | undefined;
@@ -13,14 +13,33 @@ function flipBoard() {
   boardApi?.toggleOrientation()
 }
 
+/**
+ * Undoes the last move and flips the board orientation.
+ * Safely checks if the game has started (ply > 0) to avoid errors.
+ */
+function undoLastMove() {
+  /* Example: 1.e4 e5 2.Nf3 -> ply number is 3. */
+  if (boardApi && boardApi.getCurrentPlyNumber() !== 0) {
+    flipBoard()
+    boardApi.undoLastMove()
+  }
+}
 </script>
 
 <template>
   <main>
-    <TheChessboard 
-    @board-created="(api) => (boardApi = api)" 
-    @move="flipBoard()"
+    <Chessboard 
+      @board-created="(api) => (boardApi = api)" 
+      @move="flipBoard()"
     />
-    <!-- Here button for 1. Undo and 2. SWAP -->
+    <!-- ToDo: Style for Undo and Swap Buttons. -->
+    <button @click="undoLastMove" type="button" name="Undo move">
+      Undo move 
+    </button>
+    <!-- ToDo: SWAP Button-->
+    <button @click="" type="button" name="SWAP">
+      SWAP 
+    </button>
+
   </main>
 </template>

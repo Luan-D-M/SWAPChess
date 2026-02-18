4560a74b1bf335ff373b4c97133f5342777ef15e
Luan-D-M <digjohnsons@gmail.com>
2026-02-18 12:02:28 -0300

    Adds chessboard that flips at each move (for Local playing)

--- frontend/src/views/LocalPlayView.vue
+++ frontend/src/views/LocalPlayView.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { TheChessboard, type BoardApi } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+
+let boardApi: BoardApi | undefined;
+
+onMounted(() => {
+  console.log(boardApi?.getBoardPosition());
+});
+
+function flipBoard() {
+  boardApi?.toggleOrientation()
+}
+
 </script>
 
 <template>
   <main>
-    Local Play View
+    <TheChessboard 
+    @board-created="(api) => (boardApi = api)" 
+    @move="flipBoard()"
+    />
+    <!-- Here button for 1. Undo and 2. SWAP -->
   </main>
 </template>

91e5c520022717e07f6d133456b8e232bcaeebdd
Luan-D-M <digjohnsons@gmail.com>
2026-02-19 16:17:36 -0300

    Refactor undoLastMove and fixes a bug where variable swapHappened was not being set to false after board reset

--- frontend/src/views/LocalPlayView.vue
+++ frontend/src/views/LocalPlayView.vue
@@ -8,6 +8,7 @@ import {swapWhiteFirstMove, undoSwapMove} from '../utils';
 let boardApi: BoardApi | undefined;
 
 const swapHappened = ref(false)
+const currentPly = ref(0);   /* Needed for enabling SWAP button. */
 
 onMounted(() => {
   swapHappened.value = sessionStorage.getItem('swapHappened') === 'true'
@@ -15,14 +16,17 @@ onMounted(() => {
 
 function handleBoardCreated(api: BoardApi) {
   boardApi = api;
+
+  /* Reloads board from session Storage */
   const currentPosition = sessionStorage.getItem('currentPosition')
   if (currentPosition) {
     boardApi.loadPgn(currentPosition);
 
+    currentPly.value = boardApi.getCurrentPlyNumber()
+
     if (boardApi.getTurnColor() === 'black') {
       boardApi.toggleOrientation()
     }
-
   }
 }
 
@@ -32,44 +36,17 @@ function handleReset() {
   );
   if (isSure && boardApi) {
     boardApi.resetBoard()
+    sessionStorage.setItem('swapHappened', 'false')
     sessionStorage.setItem('currentPosition', boardApi.getPgn())
+    currentPly.value = boardApi.getCurrentPlyNumber()
   }
 }
 
 function handleMove() {
-  if (boardApi){
+  if (boardApi) {
     boardApi.toggleOrientation()
     sessionStorage.setItem('currentPosition', boardApi.getPgn())
-  }
-}
-
-/**
- * Undoes the last move and flips the board orientation.
- * Safely checks if the game has started (ply > 0) to avoid errors.
- */
-function undoLastMove() {
-  /* Example: 1.e4 e5 2.Nf3 -> ply number is 3. */
-
-  if (boardApi && boardApi.getCurrentPlyNumber() !== 0) {
-    if (boardApi.getCurrentPlyNumber() === 1) {
-      boardApi.resetBoard()
-      sessionStorage.setItem('currentPosition', boardApi.getPgn())
-
-      return
-
-    } else if (swapHappened.value && boardApi.getCurrentPlyNumber() === 2) {
-      const positionBeforeSwap = undoSwapMove(boardApi.getFen())
-      boardApi.setPosition(positionBeforeSwap)
-
-      swapHappened.value = false
-      sessionStorage.setItem('swapHappened', 'false')
-    }
-    else {
-      boardApi.undoLastMove()
-    }
-
-    sessionStorage.setItem('currentPosition', boardApi.getPgn())
-    boardApi.toggleOrientation()
+    currentPly.value = boardApi.getCurrentPlyNumber()
   }
 }
 
@@ -82,11 +59,53 @@ function swap() {
     swapHappened.value = true
     sessionStorage.setItem('swapHappened', 'true')
     sessionStorage.setItem('currentPosition', boardApi.getPgn())
+
+    currentPly.value = boardApi.getCurrentPlyNumber()
   }
 }
 
+/**
+ * Undoes the last move and flips the board orientation.
+ * Safely checks if the game has started (ply > 0) to avoid errors.
+ */
+function undoLastMove() {
+  /* Example: 1.e4 e5 2.Nf3 -> ply number is 3. */
+  if (!boardApi) return;
 
+  const ply = boardApi.getCurrentPlyNumber();
+  /* Do nothing if the game hasn't started */
+  if (ply === 0) return;
+  
+  /*
+    Reset if ply=1 is needed because the swap loads a new FEN to the board,
+    so the history of moves (the first white move) is erased.
+  */
+  if (ply === 1) {
+    boardApi.resetBoard()
+    sessionStorage.setItem('currentPosition', boardApi.getPgn())
+    currentPly.value = ply - 1;  /* Gets updated ply after undoing the move */
 
+    return
+  }
+
+  const isUndoingSwap = (
+    swapHappened.value && ply === 2
+  )
+  if (isUndoingSwap) {
+    const positionBeforeSwap = undoSwapMove(boardApi.getFen())
+    boardApi.setPosition(positionBeforeSwap)
+
+    swapHappened.value = false
+    sessionStorage.setItem('swapHappened', 'false')
+
+  } else {
+    boardApi.undoLastMove()
+  }
+
+  boardApi.toggleOrientation()
+  sessionStorage.setItem('currentPosition', boardApi.getPgn())
+  currentPly.value = ply - 1  /* Gets updated ply after undoing the move */
+}
 </script>
 
 <template>
@@ -99,13 +118,18 @@ function swap() {
       Reset
     </button>
 
-    <!-- ToDo: Style for Undo and Swap Buttons. -->
+    <!-- ToDo: Style for Buttons. -->
     <button @click="undoLastMove()" type="button" name="Undo move">
       Undo move 
     </button>
-    <!-- ToDo: Enable SWAP Button only when SWAP is possible-->
-    <button @click="swap()" type="button" name="SWAP">
-      SWAP 
+    <!-- ToDo: Red styling when SWAP button is enabled.-->
+    <button 
+      @click="swap()" 
+      type="button" 
+      name="SWAP" 
+      :disabled="currentPly !== 1"
+    >
+      SWAP
     </button>
 
   </main>
